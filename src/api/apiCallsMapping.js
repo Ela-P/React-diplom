f8a492fe251e13bfc7d4dc5e1785a9118737e902
Elena P <elaina.prokudova@gmail.com>
2021-03-08 21:53:59 +0300

    Setting dependence

---
+++ src/api/apiCallsMapping.js
@@ -0,0 +1,13 @@
+const apiCallMapping = (action) => {
+    const mapping ={
+
+    };
+
+    if (!mapping.hasOwnProperty(action.type)) {
+        throw 'Not mapped action';
+    }
+
+    return mapping[action.type];
+};
+
+export default apiCallsMapping;
---
+++ src/routs/Routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import { ROUTES } from "./routeNames";
+
+
+
+const Routes = () => {
+    return (
+        <Switch>
+            <Route exact path={} component={} />
+            <Route path={} component={} />
+        </Switch>
+    );
+}
+
+export default Routes;
